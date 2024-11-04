91414ea6ec5e68e43ef52a2b53b0b6687f1bc6a8
Ometh Chandrasekara <139566507+Ometh4049@users.noreply.github.com>
2024-11-04 22:09:58 +0530

    InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,38 +1,32 @@
 public class InsertionSort {
-    // Method to perform Insertion Sort
-    public static void insertionSort(int[] arr) {
+    
+    
+    public void insertionSort(int[] arr) {
         int n = arr.length;
-        for (int i = 1; i < n; ++i) {
-            int key = arr[i];
-            int j = i - 1;
-
-            // Move elements of arr[0..i-1], that are greater than key,
-            // to one position ahead of their current position
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j = j - 1;
+        for (int i = 1; i < n; i++) {
+            int j = i;
+            while (j > 0 && arr[j - 1] > arr[j]) {
+                
+                int temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+                j--;
             }
-            arr[j + 1] = key;
         }
     }
 
-    // Method to print the array
-    public static void printArray(int[] arr) {
-        for (int j : arr) {
-            System.out.print(j + " ");
+    
+    public void insertionSortDescending(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int j = i;
+            while (j > 0 && arr[j - 1] < arr[j]) {
+                
+                int temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+                j--;
+            }
         }
-        System.out.println();
-    }
-
-    // Main method to test the sorting algorithm
-    public static void main(String[] args) {
-        int[] arr = { 5, 10, 2, 9, 3 };
-        System.out.println("Original Array:");
-        printArray(arr);
-
-        insertionSort(arr);
-
-        System.out.println("Sorted Array:");
-        printArray(arr);
     }
 }
